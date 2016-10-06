72cd065a216fb929ab0317109d1f9a96d356f598
Spokeek <Spokeek>
2016-10-06 08:24:19 +0200

    some others updates

--- app/controllers/app/login.js
+++ app/controllers/app/login.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  email: "",
+  password: "",
+  canNotLogin: function () {
+    var test = this.get('email') == "" || this.get('password') == "";
+    return test?true:null;
+  }.property("email", "password"),
+  actions: {
+    login: function () {
+      alert('erreur de connexion');
+    }
+  },
 });
